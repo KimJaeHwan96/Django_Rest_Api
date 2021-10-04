e2246cc96e62c628f3f78c502a006732783be92a
KimJaeHwan <lazysoul14@gmail.com>
2021-10-04 15:21:57 +0900

    test 를 모아두는 tests_case 디렉터리 생성 및 테스트 settings 환경 생성

---
+++ base/tests_case/test_create_TestModel.py
@@ -0,0 +1,25 @@
+
+from unittest import TestCase
+
+import pytest
+from django.utils import timezone
+
+from base.models import TestModel
+
+
+def validate_datetime_year_to_day(datetime1, datetime2):
+    assert datetime1.year == datetime2.year
+    assert datetime1.month == datetime2.month
+    assert datetime1.day == datetime2.day
+
+
+@pytest.mark.django_db
+class Test(TestCase):
+    def test_create_test_model(self):
+        test_model = TestModel()
+        test_model.save()
+        created_date = timezone.now()
+        last_modified_date = timezone.now()
+        validate_datetime_year_to_day(test_model.created_date, created_date)
+        validate_datetime_year_to_day(test_model.last_modified_date, last_modified_date)
+
