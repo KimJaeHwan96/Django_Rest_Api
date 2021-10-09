189396cbc8d3c9def5522c7dfdc90dfa1038e980
KimJaeHwan <lazysoul14@gmail.com>
2021-10-09 18:12:46 +0900

    추상 모델 클래스 BaseModel 생성
    - 기본적으로 적용할 필드를 모을 클래스
    - 모든 모델 클래스에 적용할 예정

--- base/models.py
+++ base/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.db.models import DateTimeField
 
 
-class TestModel(models.Model):
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
     created_date = DateTimeField(auto_now=True)
     last_modified_date = DateTimeField(auto_now=True)
+
+
+class TestModel(BaseModel):
+    pass
