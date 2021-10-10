a0b062b16b9bdee5ebece84d052fc379ba7c82e7
KimJaeHwan <lazysoul14@gmail.com>
2021-10-10 15:22:51 +0900

    테스트를 위한 모델 리네이밍으로 테스트 파일 및 코드 개선

--- base/tests_case/test_create_FakeModel.py
+++ base/tests_case/test_create_FakeModel.py
@@ -14,9 +14,9 @@ class TestModelTest(BaseTestCase):
         self.assertEqual(datetime1.day == datetime2.day, True)
 
     def test_create_test_model(self):
-        test_model = FakeModel()
-        test_model.save()
+        fake_model = FakeModel()
+        fake_model.save()
         created_date = timezone.now()
         last_modified_date = timezone.now()
-        self.validate_datetime_year_to_day(test_model.created_date, created_date)
-        self.validate_datetime_year_to_day(test_model.last_modified_date, last_modified_date)
+        self.validate_datetime_year_to_day(fake_model.created_date, created_date)
+        self.validate_datetime_year_to_day(fake_model.last_modified_date, last_modified_date)
