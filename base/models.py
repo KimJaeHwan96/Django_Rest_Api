79d6e52ccd2558ab220f4fb2b9bdf69f3ea76c8d
KimJaeHwan <lazysoul14@gmail.com>
2021-10-26 20:48:14 +0900

    TimeStamp 모델 클래스 생성
    - 생성일과 최근 변경일인 created_date, last_modified_date 에 대한 모델

--- base/models.py
+++ base/models.py
@@ -4,12 +4,17 @@ from django.db.models import DateTimeField
 from base.manager import BaseManager
 
 
-class BaseModel(models.Model):
+class TimeStamp(models.Model):
     class Meta:
         abstract = True
 
     created_date = DateTimeField(auto_now=True)
     last_modified_date = DateTimeField(auto_now_add=True)
+
+
+class BaseModel(TimeStamp):
+    class Meta:
+        abstract = True
 
 
 class FakeModel(BaseModel):
